20b63e7410f54c784588db7671d7737ce20b14f2
gauravshinde1 <shinde786gaurav@gmail.com>
2024-07-06 21:27:57 +0530

    complete signin page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -78,7 +78,7 @@ const SignUp = () => {
               <Label value="Your password" />
               <TextInput
                 type="password"
-                placeholder="Password"
+                placeholder="*************"
                 id="password"
                 onChange={handleChange}
               />
