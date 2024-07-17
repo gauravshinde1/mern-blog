e4ed22510ab18cdb4d1f95eec951e5e3ea1ca730
gauravshinde1 <shinde786gaurav@gmail.com>
2024-07-17 19:11:21 +0530

    Complete Delete Post api

--- api/controllers/post.controllers.js
+++ api/controllers/post.controllers.js
@@ -66,3 +66,16 @@ export const getPosts = async (req, res, next) => {
     next(err);
   }
 };
+
+export const deletepost = async (req, res, next) => {
+  console.log("delete", req.params);
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this post"));
+  }
+  try {
+    await Post.findByIdAndDelete(req.params.postId);
+    res.status(200).json("The post has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
